f1f0595e7e32553dd38f2757229404f6fc212863
prashanthKumar0 <31966594+PrashanthKumar0@users.noreply.github.com>
2024-02-11 10:23:18 +0530

    Added pass / fail status

--- src/components/ReportCard.jsx
+++ src/components/ReportCard.jsx
@@ -1,10 +1,10 @@
-import { Card, CardBody, CardHeader } from '@nextui-org/card'
+import { Card, CardBody, CardHeader, CardFooter } from '@nextui-org/card'
 import { Divider, ScrollShadow } from '@nextui-org/react';
 import React from 'react'
 import QuestionCard from './QuestionCard';
 
 function ReportCard({ questions }) {
-    
+
     const getNumCorrect = () => {
         let numCorrect = 0;
         questions.forEach(q => {
@@ -13,23 +13,35 @@ function ReportCard({ questions }) {
         return numCorrect;
     }
 
+    const getFeedback = () => {
+        return (getNumCorrect() / questions.length) >= 0.6 ? 'pass' : 'fail';
+    }
+
     return (
         <ScrollShadow className='w-full px-8'>
             <div className="flex flex-col gap-10 mx-auto max-w-[300px]">
                 <Card className="min-w-[300px]">
                     <CardHeader>
-                        <div className='text-sm opacity-50'>
+                        <div className='text-sm opacity-50 mx-auto'>
                             Your Report
                         </div>
                     </CardHeader>
 
                     <Divider />
 
-                    <CardBody className='mx-auto flex items-center'>
-                        SCORE : {getNumCorrect()} of {questions.length}
+                    <CardBody className='mx-auto flex flex-col gap-4 items-center'>
+                        <div className='flex'>
+                            SCORE : {getNumCorrect()} of {questions.length}
+                        </div>
                     </CardBody>
 
                     <Divider />
+
+                    <CardFooter>
+                        <div className={`text-sm opacity-50 mx-auto ${getFeedback() == 'pass' ? 'text-green-400' : 'text-red-500'}`}>
+                            You've {getFeedback()}ed The Test.
+                        </div>
+                    </CardFooter>
                 </Card>
                 {
                     questions.map((question, idx) => (
