0a673a2052486b98a2349194af9c57523fa24a11
prashanthKumar0 <31966594+PrashanthKumar0@users.noreply.github.com>
2024-02-11 11:20:37 +0530

    Improved Nav Drawer

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import { Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button, NavbarMenuItem, NavbarMenuToggle, NavbarMenu, Popover, PopoverTrigger, PopoverContent, User } from "@nextui-org/react";
+import { Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button, NavbarMenuItem, NavbarMenuToggle, NavbarMenu } from "@nextui-org/react";
 import Logo from "./Logo";
-// import {AcmeLogo} from "./AcmeLogo.jsx";
+
 
 const NavBar = () => {
     const [isMenuOpen, setIsMenuOpen] = React.useState(false);
@@ -28,11 +28,10 @@ const NavBar = () => {
 
             {/* text-foreground */}
 
-            <NavbarMenu className="bg-transparent">
-
+            <NavbarMenu className="bg-transparent flex flex-cols items-center gap-5 py-10">
                 <NavbarMenuItem>
                     <Link
-                        className="w-full text-white/70"
+                        className="w-full text-white/70 hover:scale-90 transition-all"
                         href="/"
                         size="lg"
                     >
@@ -43,16 +42,13 @@ const NavBar = () => {
 
                 <NavbarMenuItem>
                     <Link
-                        className="w-full text-white/70"
+                        className="w-full text-white/70 hover:scale-90 transition-all"
                         href="/quizz"
                         size="lg"
                     >
                         Quizz
                     </Link>
                 </NavbarMenuItem>
-
-
-
             </NavbarMenu>
 
         </Navbar>
